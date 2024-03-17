5ab0a8734fe0f055d744e979600de25953f2503b
ermolaevv <ermolaevvladislav1485@gmail.com>
2024-03-17 22:53:52 +0300

    Fixed Polinom Calculate: vector out of range

--- polinom/include/Polinom.h
+++ polinom/include/Polinom.h
@@ -273,8 +273,13 @@ double TPolinom::Calculate(const int countVar, const double* value)
     std::vector<std::string> valNames = expr.GetOperands();
 
     std::map<std::string, double> op;
-    for (int i = 0; i < this->countVar; i++) {
-        op.insert(std::pair<std::string, double>(valNames[i], value[i]));
+    int i = 0;
+    for (auto valName : valNames) {
+        if (i >= countVar)
+            break;
+
+        op.insert(std::pair<std::string, double>(valName, value[i]));
+        i++;
     }
 
     return expr.Calculate(op);
