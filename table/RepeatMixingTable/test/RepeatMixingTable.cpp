f1ff78fb9f3e9e8fe44aa4a494ab3055815d6050
Cufeed <gusman3000@mail.ru>
2024-03-22 20:15:30 +0300

    Hash tables. Added some new tests for RepeatMixingTable by Cufeed

--- table/RepeatMixingTable/test/RepeatMixingTable.cpp
+++ table/RepeatMixingTable/test/RepeatMixingTable.cpp
@@ -14,3 +14,16 @@ TEST(RepeatMixingTable, Delete) {
     table.Delete(2);
     ASSERT_THROW(table.Find(2), std::runtime_error);
 }
+
+TEST(RepeatMixingTable, FindNonExistentKey) {
+    RepeatMixingTable<int, string> table;
+    ASSERT_THROW(table.Find(100), std::runtime_error);
+}
+
+TEST(RepeatMixingTable, InsertFullTable) {
+    RepeatMixingTable<int, string> table(3);
+    table.Insert(1, "one");
+    table.Insert(2, "two");
+    table.Insert(3, "three");
+    ASSERT_THROW(table.Insert(4, "four"), std::runtime_error);
+}
