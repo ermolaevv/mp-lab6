c06772921a62dc96238255db3fe4b16bfd32bb96
Cufeed <gusman3000@mail.ru>
2024-03-22 18:34:47 +0300

    Hash tables with the same errors (for checking)

--- table/ChainHashTable/test/test_ChainHashTable.cpp
+++ table/ChainHashTable/test/test_ChainHashTable.cpp
@@ -1,3 +1,23 @@
-#include <gtest/gtest.h>
-#include "ChainHashTable.h"
-using namespace std;
+    #include <gtest/gtest.h>
+    #include "ChainHashTable.h"
+    using namespace std;
+
+    TEST(ChainHashTable, InsertAndFind) {
+        ChainHashTable<int, string> hashTable;
+        int key = 1;
+        string value = "test";
+        hashTable.Insert(key, value);
+        ASSERT_EQ(*hashTable.Find(key), value);
+    }
+
+    TEST(ChainHashTable, Delete) {
+        ChainHashTable<int, string> hashTable;
+        hashTable.Insert(5, "test5");
+        hashTable.Delete(5);
+        ASSERT_THROW(hashTable.Find(5), runtime_error);
+    }
+
+    TEST(ChainHashTable, FindNonExistentKey) {
+        ChainHashTable<int, string> hashTable;
+        ASSERT_THROW(hashTable.Find(100), runtime_error);
+    }
--- table/RepeatMixingTable/test/RepeatMixingTable.cpp
+++ table/RepeatMixingTable/test/RepeatMixingTable.cpp
@@ -1,3 +1,16 @@
 #include <gtest/gtest.h>
 #include "RepeatMixingTable.h"
 using namespace std;
+
+TEST(RepeatMixingTable, InsertAndFind) {
+    RepeatMixingTable<int, string> table;
+    table.Insert(1, "one");
+    ASSERT_EQ(*table.Find(1), "one");
+}
+
+TEST(RepeatMixingTable, Delete) {
+    RepeatMixingTable<int, string> table;
+    table.Insert(2, "two");
+    table.Delete(2);
+    ASSERT_THROW(table.Find(2), std::runtime_error);
+}
